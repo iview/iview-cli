4c5081b37448273dd3cb3a4532d29fdb242fdf94
梁灏 <admin@aresn.com>
2017-02-12 20:55:14 +0800

    init
    init

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let logo = path.join(__dirname, 'assets/img/WeFlow.png');
+let win;
+
+function createWindow () {
+    win = new BrowserWindow({
+        width: 360,
+        height: 580,
+        title: 'iView',
+        center: true,
+        resizable: false,
+        icon: logo,
+        titleBarStyle: 'hidden'
+    });
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+
+    // 当 window 被关闭，这个事件会被触发。
+    win.on('closed', () => {
+        // 取消引用 window 对象，如果你的应用支持多窗口的话，
+        // 通常会把多个 window 对象存放在一个数组里面，
+        // 与此同时，你应该删除相应的元素。
+        win = null
+    })
+}
+
+app.on('ready', createWindow);
+
+// 检查更新
