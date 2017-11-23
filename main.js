2eb6838642546b70fbb99ad8fca6ca0362261b59
zhigang.li <zhigang.li@tendcloud.com>
2017-11-23 12:51:19 +0800

    update

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function createWindow () {
     }));
 
     // 打开开发者工具。
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     // 当 window 被关闭，这个事件会被触发。
     win.on('close', (event) => {
